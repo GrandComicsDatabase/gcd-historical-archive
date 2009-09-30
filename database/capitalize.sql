18121c2c82f64af46430244c81e7d5a3fa38425f
Henry Andrews <hha1@cornell.edu>
2009-09-30 15:20:38 +0000

    Reviewed-by: ralf.haring Approved-by: ralf.haring
    Drop useless fields and indexes, add some useful indexes, move the
    table rename stuff to a better place.

---
+++ database/capitalize.sql
@@ -0,0 +1,6 @@
+ALTER TABLE countries RENAME Countries;
+ALTER TABLE languages RENAME Languages;
+ALTER TABLE indexers RENAME Indexers;
+ALTER TABLE indexcredit RENAME IndexCredit;
+ALTER TABLE reservations RENAME Reservations;
+
--- database/migrate-prototype-1-tables.sql
+++ database/migrate-prototype-1-tables.sql
@@ -2,25 +2,88 @@ SET SESSION sql_mode='STRICT_ALL_TABLES';
 
 ALTER DATABASE CHARACTER SET utf8;
 
--- Restore original capitalization for case-sensitive filesystems.
--- This has to be done in two steps as rename won't just change case.
--- Convert tables to utf8.
--- Add flag columns.
+-- Convert tables to utf8, add flag columns, drop useles columns and indices.
 
-ALTER TABLE countries RENAME c;
-ALTER TABLE c RENAME Countries, CONVERT TO CHARACTER SET utf8;
-ALTER TABLE languages RENAME l;
-ALTER TABLE l RENAME Languages, CONVERT TO CHARACTER SET utf8;
-ALTER TABLE publishers CONVERT TO CHARACTER SET utf8;
-ALTER TABLE series CONVERT TO CHARACTER SET utf8;
-ALTER TABLE issues CONVERT TO CHARACTER SET utf8;
+ALTER TABLE Countries CONVERT TO CHARACTER SET utf8;
+ALTER TABLE Languages CONVERT TO CHARACTER SET utf8;
+
+ALTER TABLE publishers
+    DROP COLUMN Connection,
+    DROP COLUMN NextID,
+    DROP COLUMN Updated,
+    DROP COLUMN AlphaSortCode,
+    DROP COLUMN IssueCount,
+    CONVERT TO CHARACTER SET utf8;
+
+ALTER TABLE series
+    DROP COLUMN Crossref,
+    DROP COLUMN CrossrefID,
+    DROP COLUMN `File`,
+    DROP COLUMN InitDist,
+    DROP COLUMN Included,
+    DROP COLUMN Indexers,
+    DROP COLUMN LstChang,
+    DROP COLUMN oldID,
+    DROP COLUMN SelfCount,
+    DROP COLUMN Themes,
+    DROP COLUMN UpdateDist,
+    CONVERT TO CHARACTER SET utf8;
+
+ALTER TABLE issues
+    DROP COLUMN Char_App,
+    DROP COLUMN Colors,
+    DROP COLUMN CoverCheck,
+    DROP COLUMN CoverCount,
+    DROP COLUMN Feature,
+    DROP COLUMN Genre,
+    DROP COLUMN InitDist,
+    DROP COLUMN Inks,
+    DROP COLUMN isUpdated,
+    DROP COLUMN Letters,
+    DROP COLUMN LstChang,
+    DROP COLUMN Pencils,
+    DROP COLUMN rel_year,
+    DROP COLUMN Reprints,
+    DROP COLUMN Seq_No,
+    DROP COLUMN Script,
+    DROP COLUMN SelfCount,
+    DROP COLUMN Synopsis,
+    DROP COLUMN SeriesLink,
+    DROP COLUMN Title,
+    DROP COLUMN `Type`,
+    DROP COLUMN UpdateDist,
+    ADD INDEX (Issue),
+    ADD INDEX (VolumeNum),
+    ADD INDEX (Modified),
+    CONVERT TO CHARACTER SET utf8;
+
 ALTER TABLE stories
+    DROP COLUMN rel_year,
+    DROP COLUMN LstChang,
+    DROP COLUMN InitDist,
+    DROP COLUMN IssueCount,
+    DROP COLUMN Key_Date,
+    DROP COLUMN Pub_Date,
+    DROP COLUMN Price,
+    DROP COLUMN SeriesID,
+    DROP COLUMN Issue,
+    DROP COLUMN Yr_Began,
+    DROP COLUMN Bk_Name,
+    DROP COLUMN Pub_Name,
+    DROP INDEX Script,
+    DROP INDEX Pencils,
+    DROP INDEX Inks,
+    DROP INDEX Colors,
+    DROP INDEX Letters,
+    DROP INDEX Editing,
+    DROP INDEX Char_App,
     ADD COLUMN no_script tinyint(1) NOT NULL default 0,
     ADD COLUMN no_pencils tinyint(1) NOT NULL default 0,
     ADD COLUMN no_inks tinyint(1) NOT NULL default 0,
     ADD COLUMN no_colors tinyint(1) NOT NULL default 0,
     ADD COLUMN no_letters tinyint(1) NOT NULL default 0,
     ADD COLUMN page_count_uncertain tinyint(1) NOT NULL default 0,
+    ADD INDEX (`type`),
     ADD INDEX (no_script),
     ADD INDEX (no_pencils),
     ADD INDEX (no_inks),
@@ -28,13 +91,11 @@ ALTER TABLE stories
     ADD INDEX (no_letters),
     ADD INDEX (page_count_uncertain),
     CONVERT TO CHARACTER SET utf8;
+
 ALTER TABLE covers CONVERT TO CHARACTER SET utf8;
-ALTER TABLE reservations RENAME r;
-ALTER TABLE r RENAME Reservations, CONVERT TO CHARACTER SET utf8;
-ALTER TABLE indexers RENAME i;
-ALTER TABLE i RENAME Indexers, CONVERT TO CHARACTER SET utf8;
-ALTER TABLE indexcredit RENAME ic;
-ALTER TABLE ic RENAME IndexCredit, CONVERT TO CHARACTER SET utf8;
+ALTER TABLE Reservations CONVERT TO CHARACTER SET utf8;
+ALTER TABLE Indexers CONVERT TO CHARACTER SET utf8;
+ALTER TABLE IndexCredit CONVERT TO CHARACTER SET utf8;
 
 UPDATE stories SET script='', no_script=1 WHERE script IN (
     'none',  -- English
