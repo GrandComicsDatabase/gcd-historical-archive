b7b7295c6aef4b138c87fb42b487f40dadc67393
Henry Andrews <hha1@cornell.edu>
2009-09-30 15:19:18 +0000

    Reviewed-by: ralf.haring, jgarcke Approved-by: jgarcke
    Turn off the OI in the prototype branch.  Leave login for use with covers.

--- branches/prototype/pydjango/settings.py
+++ branches/prototype/pydjango/settings.py
@@ -50,9 +50,7 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    # 'django.contrib.sites',
     'django.contrib.admin',
-    'apps.oi',
     'apps.gcd',
 )
 
