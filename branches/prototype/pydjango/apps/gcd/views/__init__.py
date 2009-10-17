e6a02d90904644037e3c66b944b05ec67e23524e
Jochen Garcke <gcdmail@garcke.de>
2009-10-17 17:58:33 +0000

    rev 162, different pagination code

--- branches/prototype/pydjango/apps/gcd/views/__init__.py
+++ branches/prototype/pydjango/apps/gcd/views/__init__.py
@@ -5,6 +5,8 @@ from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
+from pagination import DiggPaginator
+
 ORDER_ALPHA = "alpha"
 ORDER_CHRONO = "chrono"
 
@@ -20,14 +22,15 @@ def index(request):
                               context_instance=RequestContext(request))
       
 
-PAGE_DELTA = 2
 def paginate_response(request, queryset, template, vars, page_size=100,
                       callback_key=None, callback=None):
     """
-    Quick hack to implement direct page links, needs revisiting later
-    or we should reconsider using another library such as DiggPaginator.
+    Uses DiggPaginator from
+    http://bitbucket.org/miracle2k/djutils/src/tip/djutils/pagination.py.
+    We could reconsider writing our own code.
     """
-    p = QuerySetPaginator(queryset, page_size)
+
+    p = DiggPaginator(queryset, page_size, body=5, padding=2)
 
     page_num = 1
     redirect = None
@@ -49,29 +52,10 @@ def paginate_response(request, queryset, template, vars, page_size=100,
 
     page = p.page(page_num)
 
-    page_min = page_num - PAGE_DELTA
-    low_page_range = ()
-    if page_min <= 1 + PAGE_DELTA:
-        page_min = 1
-    else:
-        low_page_range = range(1, PAGE_DELTA +1)
-
-    page_max = page_num + PAGE_DELTA
-    high_page_range = ()
-    if page_max >= p.num_pages - PAGE_DELTA:
-        page_max = p.num_pages
-    else:
-        high_page_range = range(p.num_pages - PAGE_DELTA +1,
-                                p.num_pages + 1)
     vars['items'] = page.object_list
     vars['paginator'] = p
     vars['page'] = page
     vars['page_number'] = page_num
-    vars['ranges'] = { 'low': low_page_range,
-                       'current': range(page_min, page_max + 1),
-                       'current_max': page_max,
-                       'high': high_page_range,
-                     }
 
     if callback_key is not None:
         vars[callback_key] = callback(page)
