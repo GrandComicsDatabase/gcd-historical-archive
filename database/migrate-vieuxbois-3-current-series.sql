8f4bc4fb53f3059d588a91b3c861c095345c8a32
Henry Andrews <hha1@cornell.edu>
2009-12-06 04:10:06 +0000

    http://dev.comics.org/reviews/r/321

---
+++ database/migrate-vieuxbois-3-current-series.sql
@@ -0,0 +1,444 @@
+UPDATE gcd_series SET is_current=1 WHERE id IN (
+    87, -- Detective Comics
+    97, -- Action Comics
+    32602, -- Adventure Comics
+    12883, -- Desolation Jones
+    13650, -- Fell
+    17010, -- Invincible
+    15174, -- Walking Dead
+    10228, -- Age of Bronze
+    15593, -- Fullmetal Alchemist
+    36742, -- Naoki Urasawa's 20th Century Boys
+    37275, -- Black Jack
+    2605, -- Uncanny X-Men, The
+    18602, -- Uncanny X-Men Annual
+    11653, -- Astonishing X-Men
+    37856, -- X-Men Forever
+    29823, -- X-Men: Legacy
+    39631, -- X-Men: Legacy Annual
+    16465, -- X-Factor
+    29506, -- X-Force
+    4905, -- Simpsons Comics
+    15318, -- Simpsons Comics Presents Bart Simpson
+    15319, -- Bongo Comics Presents Futurama Comics
+    17986, -- Bongo Comics Presents Simpsons Super Spectacular
+    25083, -- Simpsons Summer Shindig, The
+    20796, -- Simpsons Winter Wing Ding, The
+    5435, -- Treehouse of Horror
+    31882, -- Star Wars The Clone Wars
+    19003, -- Star Wars: Dark Times
+    37846, -- Star Wars: Invasion
+    18185, -- Star Wars: Legacy
+    21518, -- Buffy the Vampire Slayer Season Eight
+    31174, -- Conan the Cimmerian
+    30167, -- End League, The
+    39632, -- Beasts of Burden
+    5623, -- Usagi Yojimbo
+    26039, -- Goon, The
+    10447, -- Archie
+    13245, -- Archie & Friends
+    12503, -- Archie Comics Digest
+    17930, -- Archie's Double Digest
+    13247, -- Archie's Pal Jughead Comics
+    17931, -- Archie's Pals 'N' Gals Double Digest Magazine
+    11969, -- Betty and Veronica
+    17975, -- Betty and Veronica Digest Magazine
+    17932, -- Betty and Veronica Double Digest Magazine
+    29709, -- Zorro
+    18019, -- Jughead & Friends Digest Magazine
+    19520, -- Lone Ranger, The
+    11968, -- Betty
+    25058, -- Boys, The
+    35798, -- Buck Rogers
+    7875, -- Sabrina
+    12006, -- Sonic The Hedgehog
+    29889, -- Echo
+    29793, -- RASL
+    15653, -- Tales From Riverdale digest
+    32591, -- Baby & Me
+    13040, -- Veronica
+    27557, -- Bleach
+    19958, -- Case Closed
+    17973, -- Jughead's Double Digest
+    31303, -- Eyeshield 21
+    34028, -- Fushigi Yugi: Genbu Kaiden
+    34741, -- Gun Blaze West
+    38319, -- High School Debut
+    38863, -- Hikaru No Go
+    38861, -- Hoshin Engi
+    38847, -- Ikigami
+    19895, -- InuYasha
+    27115, -- Jojo's Bizarre Adventure
+    38855, -- Kekkaishi
+    38857, -- Muhyo & Roji's Bureau of Supernatural Investigation
+    31541, -- Sonic The Hedgehog Archives
+    25323, -- Naruto
+    31321, -- Real
+    38854, -- Yakitate!! Japan
+    38853, -- Yu-Gi-Oh! GX
+    34027, -- Yumekui Kenbun: Nightmare Inspector
+    26809, -- YuYu Hakusho
+    31417, -- Alive: The Final Evolution
+    37240, -- Aventura
+    31881, -- Fairy Tail
+    39342, -- Ghost Hunt
+    28225, -- Negima!
+    18884, -- Tsubasa
+    28548, -- xxxHOLiC
+    30695, -- glamourpuss
+    3096, -- Femforce
+    13126, -- Men of Mystery Comics
+    31217, -- Crypt of Horror
+    37572, -- Absolution
+    35757, -- Phantom: Generations
+    36683, -- Batman and Robin
+    13914, -- All Star Batman & Robin, the Boy Wonder
+    20878, -- Batman Confidential
+    39891, -- Great Ten, The
+    14588, -- Jonah Hex
+    39705, -- JSA All-Stars
+    4961, -- Looney Tunes
+    33626, -- Warlord
+    97, -- Action Comics
+    32602, -- Adventure Comics
+    38961, -- Batgirl
+    26025, -- Booster Gold
+    38783, -- Doom Patrol
+    26805, -- Green Arrow / Black Canary
+    39142, -- Magog
+    32601, -- R.E.B.E.L.S.
+    36684, -- Red Robin
+    7260, -- Scooby-Doo
+    31829, -- Shield, The
+    30086, -- Super Friends
+    30370, -- Titans
+    39888, -- Azrael
+    141, -- Batman
+    36685, -- Batman: Streets of Gotham
+    33598, -- Batman: The Brave and the Bold
+    21522, -- Brave and the Bold, The
+    18427, -- Green Lantern Corps
+    18752, -- Justice League of America
+    34024, -- Outsiders
+    34078, -- Power Girl
+    14121, -- Supergirl
+    11863, -- Superman / Batman
+    29505, -- Tiny Titans
+    31255, -- Billy Batson & the Magic of Shazam!
+    37575, -- Gotham City Sirens
+    13519, -- Green Lantern
+    20856, -- Justice Society of America
+    16530, -- Superman
+    11297, -- Teen Titans
+    39704, -- Web, The
+    17527, -- Wonder Woman
+    18723, -- – Cartoon Network Action Pack
+    29499, -- 9
+    14582, -- Gold Digger
+    11288, -- Amazing Spider-Man, The
+    37560, -- Captain America
+    33502, -- Dark Avengers
+    23407, -- Nova
+    38919, -- Ultimate Comics Avengers
+    38918, -- Ultimate Comics Spider-Man
+    33758, -- Black Panther
+    29892, -- Cable
+    39233, -- Daredevil
+    32043, -- Deadpool
+    31452, -- Marvel Adventures Super Heroes
+    23629, -- Marvel Adventures Spider-Man
+    39711, -- Spider-Woman
+    38385, -- Uncanny X-Men: First Class
+    39600, -- Vengeance of the Moon Knight
+    37923, -- Deadpool: Merc With A Mouth
+    38633, -- Incredible Hulk
+    22524, -- Mighty Avengers
+    33759, -- Punisher
+    17522, -- Thunderbolts
+    33410, -- War Machine
+    17574, -- Wolverine: Origins
+    36479, -- Wolverine Weapon X
+    23309, -- Avengers: The Initiative
+    38664, -- Dark Wolverine
+    38664, -- Dark Wolverine
+    11218, -- Fantastic Four
+    30802, -- Guardians of the Galaxy
+    28683, -- Hulk
+    28772, -- Incredible Hercules
+    30723, -- Invincible Iron Man
+    16781, -- Ms. Marvel
+    12464, -- New Avengers
+    36520, -- New Mutants
+    33729, -- Secret Warriors
+    30390, -- Wolverine: First Class
+    17671, -- New Avengers Annual
+    33987, -- Thor
+    18318, -- Robin Archives
+    18324, -- Wonder Woman Archives
+    18322, -- Superman: The World's Finest Archives
+    18322, -- Superman: The World's Finest Archives
+    10959, -- Batman: The Dark Knight Archives
+    18624, -- Marvel Masterworks: Golden Age Sub-Mariner
+    27227, -- Marvel Masterworks: Nick Fury, Agent of S.H.I.E.L.D.
+    18520, -- Marvel Masterworks: Doctor Strange
+    17374, -- Marvel Masterworks: The Fantastic Four
+    29507, -- Marvel Masterworks: Golden Age Daring Mystery
+    18618, -- Marvel Masterworks: The X-Men
+    17330, -- Marvel Masterworks Atlas Era Tales To Astonish
+    31662, -- Air
+    15525, -- DMZ
+    10549, -- Fables
+    37875, -- Greek Street
+    3599, -- Hellblazer
+    30703, -- House of Mystery
+    18973, -- Jack of Fables
+    31172, -- Madame Xanadu
+    27814, -- Northlanders
+    39148, -- Sweet Tooth
+    32297, -- Unknown Soldier
+    36675, -- Unwritten, The
+    23585, -- Scalped
+    31874, -- Authority, The
+    38707, -- Free Realms
+    32187, -- Gears of War
+    37296, -- StarCraft
+    20665, -- Stormwatch: P.H.D.
+    31449, -- Wildcats
+    27430, -- World of Warcraft
+    11981, -- Ex Machina
+    23584, -- Gen 13
+    11285, -- Beano, The
+    10609, -- Commando
+    39324, -- 2000 AD
+    11293, -- 2000 AD
+    35895, -- Classics from the Comics
+    34153, -- Agent X9
+    31035, -- Bamse
+    11020, -- Bamsebiblioteket
+    33481, -- Death Note
+    33528, -- Elvis
+    31232, -- Ernie
+    32007, -- Fantomen
+    33548, -- Hälge [julalbum]
+    33549, -- Hälge: Presentbok
+    21747, -- Agent X9
+    22155, -- Billy
+    22205, -- Billy julehefte
+    22201, -- Billy Klassiske Helsider
+    22187, -- Billy [Bilag til Billy]
+    22248, -- Blondie [Blondie julehefte]
+    22291, -- Boing
+    22633, -- Boing pocket
+    22313, -- Bone
+    29828, -- Buddha
+    33533, -- Herman Hedning: Samlade serier
+    34263, -- I.N.V.U.
+    33572, -- Inu Yasha
+    34194, -- Kalle Anka & C:o
+    34431, -- Kalle Anka & C:o - den kompletta årgången
+    39396, -- Carl Barks' jul
+    22487, -- Conan
+    22510, -- Conan Krønikene
+    36946, -- Conan Maxi
+    21200, -- Donald Duck & Co
+    39045, -- Donald Duck & Co De komplette årgangene
+    36470, -- Donald Duck & Co julehefte
+    39698, -- Donald Duck Ekstra
+    39162, -- Donald Ducks Show
+    39133, -- Donald Pocket
+    37403, -- EON
+    28404, -- Eon julehefte
+    22757, -- Ernie julespesial
+    31229, -- Kalle Anka och hans vänner önskar god jul
+    34050, -- Kalle Ankas pocket
+    22801, -- Eventyrprinsesser
+    22897, -- Fantomet
+    22958, -- Fantomets krønike
+    23052, -- Fiinbeck og Fia
+    25753, -- Hårek julehefte
+    24038, -- Herman Hedning
+    36123, -- Humorparaden
+    25770, -- Illustrerte Klassikere
+    34283, -- Kare First Love
+    34368, -- Lee Falk's Fantomen - Den inbundna årgången
+    32727, -- Mästerdetektiven Conan
+    31294, -- Penny
+    33681, -- Samurai Deeper Kyo
+    32163, -- Schassen och Knasen
+    33696, -- Spirous äventyr
+    34474, -- Starcraft Frontline
+    35312, -- unge Spirou, Den
+    31295, -- Wendy
+    26276, -- Jens von Bustenskjold [Bustenskjold julehefte]
+    27790, -- Kapteinens jul
+    29604, -- Knoll og Tott
+    31008, -- Kollektivet
+    37579, -- Kollektivet bok
+    12270, -- Kollektivet Julealbum [Kollektivet Julehefte]
+    29915, -- Lars Lauvik bok
+    23449, -- Larsons gale verden
+    29896, -- Larsons gale verden høstspesial
+    29898, -- Larsons Gale Verden julespesial
+    35634, -- Larsons Gale Verden påskespesial
+    29897, -- Larsons Gale Verden sommerspesial
+    29895, -- Larsons Gale Verden vinterspesial
+    21320, -- M
+    21891, -- M Julealbum [M julehefte]
+    30151, -- M [M-bok]
+    36591, -- Maxi Tex
+    39138, -- Mikke Mus & Langbein julehefte
+    36108, -- Nana
+    12280, -- Nemi
+    12279, -- Nemi julehefte
+    36185, -- Nemi kalender
+    36145, -- Nemi [Nemi bokserien]
+    21575, -- Pondus
+    36630, -- Pondus bok
+    37065, -- Pondus dag til dag kalender
+    36631, -- Pondus eliteserien
+    29901, -- Pondus Julehefte
+    36648, -- Pondus kalender
+    36718, -- Prins Valiant
+    36728, -- Prins Valiant bok
+    36719, -- Prins Valiant [hardcover]
+    36783, -- Pusur julehefte
+    21529, -- Rex Rudi bok
+    37277, -- Rocky
+    37285, -- Rocky bok
+    37278, -- Rocky julehefte [Rocky julespesial]
+    37377, -- Scooby-Doo
+    37383, -- Scooby-Doo julealbum
+    37507, -- Serie-pocket
+    37765, -- Skipper'n julehefte [Skippern julehefte]
+    37830, -- Sleivdal julehefte
+    37851, -- Sleivdal-album
+    38473, -- Smörbukk [Smørbukk]
+    39522, -- Snøfte Smith
+    23649, -- Spider-Man
+    23475, -- Spider-Man Kids
+    35933, -- Sprint boksamling
+    25378, -- Tex Willer
+    31359, -- Tex Willer Spesial
+    34162, -- Tommy og Tigern julehefte [Tommy & Tigern julehefte]
+    10092 -- MAD
+);
+
+SET @adross=(SELECT id FROM auth_user
+    WHERE first_name='Allen' AND last_name='Ross');
+SET @dbest=(SELECT id FROM auth_user
+    WHERE first_name='Dwayne' AND last_name='Best');
+SET @ddmilne=(SELECT id FROM auth_user
+    WHERE first_name='Donald Dale' AND last_name='Milne');
+SET @gfischer=(SELECT id FROM auth_user
+    WHERE first_name='Gregory' AND last_name='Fischer');
+SET @gwhitmore=(SELECT id FROM auth_user
+    WHERE first_name='Gregg' AND last_name='Whitmore');
+SET @handrews=(SELECT id FROM auth_user
+    WHERE first_name='Henry' AND last_name='Andrews');
+SET @jgarcke=(SELECT id FROM auth_user
+    WHERE first_name='Jochen' AND last_name='Garcke');
+SET @jrvandore=(SELECT id FROM auth_user
+    WHERE first_name='Jim' AND last_name='Van Dore');
+SET @klangsmith=(SELECT id FROM auth_user
+    WHERE first_name='Kelly' AND last_name='Langston-Smith');
+SET @lmazzella=(SELECT id FROM auth_user
+    WHERE first_name='Lou' AND last_name='Mazzella');
+SET @mdewally=(SELECT id FROM auth_user
+    WHERE first_name='Michael' AND last_name='Dewally');
+SET @mhofmann=(SELECT id FROM auth_user
+    WHERE first_name='Matthias' AND last_name='Hofmann');
+SET @rharing=(SELECT id FROM auth_user
+    WHERE first_name='Ralf' AND last_name='Haring'
+    ORDER BY id LIMIT 1);
+SET @rschenk=(SELECT id FROM auth_user
+    WHERE first_name='Ramon' AND last_name='Schenk');
+SET @trose=(SELECT id FROM auth_user
+    WHERE first_name='Tony R.' AND last_name='Rose');
+
+INSERT INTO oi_ongoing_reservation (indexer_id, series_id) VALUES
+    (@adross, 97), -- , -- Action Comics
+    (@adross, 13914), -- All Star Batman & Robin, the Boy Wonder
+    (@adross, 16530), -- Superman
+
+    (@dbest, 29505), -- Tiny Titans
+    (@dbest, 17522), -- Thunderbolts
+
+    (@ddmilne, 10092), -- MAD
+
+    (@gfischer, 4905), -- Simpsons Comics
+    (@gfischer, 15318), -- Simpsons Comics Presents Bart Simpson
+    (@gfischer, 31882), -- Star Wars The Clone Wars
+    (@gfischer, 19003), -- Star Wars: Dark Times
+    (@gfischer, 37846), -- Star Wars: Invasion
+    (@gfischer, 18185), -- Star Wars: Legacy
+    (@gfischer, 38961), -- Batgirl
+    (@gfischer, 141), -- Batman
+    (@gfischer, 36683), -- Batman and Robin
+    (@gfischer, 20878), -- Batman Confidential
+    (@gfischer, 36685), -- Batman: Streets of Gotham
+    (@gfischer, 21522), -- Brave and the Bold, The
+    (@gfischer, 87), -- Detective Comics
+    (@gfischer, 38783), -- Doom Patrol
+    (@gfischer, 37575), -- Gotham City Sirens
+    (@gfischer, 34024), -- Outsiders
+    (@gfischer, 36684), -- Red Robin
+    (@gfischer, 11863), -- Superman / Batman
+    (@gfischer, 33626), -- Warlord
+    (@gfischer, 30802), -- Guardians of the Galaxy
+    (@gfischer, 23407), -- Nova
+    (@gfischer, 16465), -- X-Factor
+
+    (@gwhitmore, 18752), -- Justice League of America
+    (@gwhitmore, 31829), -- Shield, The
+    (@gwhitmore, 39704), -- Web, The
+
+    (@handrews, 2605), -- Uncanny X-Men, The
+    (@handrews, 18602), -- Uncanny X-Men Annual
+    (@handrews, 11653), -- Astonishing X-Men
+    (@handrews, 29823), -- X-Men: Legacy
+    (@handrews, 39631), -- X-Men: Legacy Annual
+    (@handrews, 29506), -- X-Force
+    (@handrews, 36520), -- New Mutants
+
+    (@jgarcke, 15525), -- DMZ
+    (@jgarcke, 23585), -- Scalped
+    (@jgarcke, 12883), -- Desolation Jones
+
+    (@jrvandore, 32602), -- Adventure Comics
+    (@jrvandore, 26025), -- Booster Gold
+    (@jrvandore, 20856), -- Justice Society of America
+    (@jrvandore, 32601), -- R.E.B.E.L.S.
+
+    (@klangsmith, 39233), -- Daredevil
+    (@klangsmith, 37856), -- X-Men Forever
+
+    (@lmazzella, 29889), -- Echo
+    (@lmazzella, 29793), -- RASL
+    (@lmazzella, 31174), -- Conan the Cimmerian
+    (@lmazzella, 31662), -- Air
+    (@lmazzella, 26805), -- Green Arrow / Black Canary
+    (@lmazzella, 27814), -- Northlanders
+    (@lmazzella, 34078), -- Power Girl
+    (@lmazzella, 14121), -- Supergirl
+    (@lmazzella, 29709), -- Zorro
+    (@lmazzella, 17010), -- Invincible
+    (@lmazzella, 15174), -- Walking Dead
+    (@lmazzella, 38918), -- Ultimate Comics Spider-Man
+    (@lmazzella, 17574), -- Wolverine: Origins
+
+    (@mdewally, 10549), -- Fables
+
+    (@mhofmann, 17527), -- Wonder Woman
+    (@mhofmann, 11288), -- Amazing Spider-Man, The
+    (@mhofmann, 11218), -- Fantastic Four
+
+    (@rschenk, 14588), -- Jonah Hex
+    (@rschenk, 23309), -- Avengers: The Initiative
+    (@rschenk, 37560), -- Captain America
+
+    (@rharing, 10228), -- Age of Bronze
+    (@rharing, 13650), -- Fell
+    (@rharing, 15593), -- Fullmetal Alchemist
+
+    (@trose, 13519); -- Green Lantern
+
