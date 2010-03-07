cc6eb850b3223095eea478218cd751f76dd04bd4
Henry Andrews <hha1@cornell.edu>
2010-03-07 07:26:17 +0000

    http://dev.comics.org/r/371/
    Deleting obsolete migration scripts and schemas.  The project went in a different
    direction than this with new fun and then the crisis-derived releases.

--- database/capitalize.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE countries RENAME Countries;
-ALTER TABLE languages RENAME Languages;
-ALTER TABLE indexers RENAME Indexers;
-ALTER TABLE indexcredit RENAME IndexCredit;
-ALTER TABLE reservations RENAME Reservations;
-
