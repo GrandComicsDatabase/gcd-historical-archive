2a2f5f331fe69c524d5dbd90e80e6404b591a06d
Henry Andrews <hha1@cornell.edu>
2010-05-25 17:52:13 +0000

    http://reviews.comics.org/r/487/
    delete obsolete scripts

--- database/scrub.sql
+++
@@ -1,9 +0,0 @@
--- This script is used to remove sensitive data from production (or replicated)
--- database dumps.  It must be run before anything else is done to the dump.
-
-SET SESSION sql_mode='STRICT_ALL_TABLES';
-
-UPDATE Indexers SET password='',
-                    eMail='indexer@comics.org',
-                    Message=NULL;
-
