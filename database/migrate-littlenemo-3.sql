e6de58a8ccd81ce7ec339fe9a2980891e0e528a1
Ralf Haring <ralf.haring@gmail.com>
2010-06-14 22:45:27 +0000

    delete covers - http://reviews.comics.org/r/500/

--- database/migrate-littlenemo-3.sql
+++ database/migrate-littlenemo-3.sql
@@ -2,3 +2,4 @@ ALTER TABLE gcd_story ADD `deleted` bool NOT NULL DEFAULT false;
 ALTER TABLE gcd_cover DROP COLUMN variant_text, DROP COLUMN variant_code;
 DELETE FROM gcd_cover where has_image = 0;
 ALTER TABLE gcd_cover DROP COLUMN has_image;
+ALTER TABLE gcd_cover ADD `deleted` bool NOT NULL DEFAULT false;
