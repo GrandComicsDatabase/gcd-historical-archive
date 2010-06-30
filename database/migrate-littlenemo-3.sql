f9599aa77942b4b373d1f1bb8fd7156d20edfa06
Ralf Haring <ralf.haring@gmail.com>
2010-06-30 23:40:59 +0000

    optional email for all approvals - http://reviews.comics.org/r/522

--- database/migrate-littlenemo-3.sql
+++ database/migrate-littlenemo-3.sql
@@ -3,3 +3,4 @@ ALTER TABLE gcd_cover DROP COLUMN variant_text, DROP COLUMN variant_code;
 DELETE FROM gcd_cover where has_image = 0;
 ALTER TABLE gcd_cover DROP COLUMN has_image;
 ALTER TABLE gcd_cover ADD `deleted` bool NOT NULL DEFAULT false;
+ALTER TABLE gcd_indexer ADD `notify_on_approve` bool NOT NULL DEFAULT false;
