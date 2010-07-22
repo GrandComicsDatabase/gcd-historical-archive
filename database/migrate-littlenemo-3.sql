bf68fd5b023fa6e3cbb4caadf7cdd80886542d5c
Ralf Haring <ralf.haring@gmail.com>
2010-07-22 04:22:17 +0000

    delete brands - http://reviews.comics.org/r/535

--- database/migrate-littlenemo-3.sql
+++ database/migrate-littlenemo-3.sql
@@ -4,3 +4,8 @@ DELETE FROM gcd_cover where has_image = 0;
 ALTER TABLE gcd_cover DROP COLUMN has_image;
 ALTER TABLE gcd_cover ADD `deleted` bool NOT NULL DEFAULT false;
 ALTER TABLE gcd_indexer ADD `notify_on_approve` bool NOT NULL DEFAULT false;
+ALTER TABLE gcd_brand ADD `deleted` bool NOT NULL DEFAULT false;
+ALTER TABLE gcd_issue ADD `deleted` bool NOT NULL DEFAULT false;
+ALTER TABLE gcd_series ADD `deleted` bool NOT NULL DEFAULT false;
+ALTER TABLE gcd_publisher ADD `deleted` bool NOT NULL DEFAULT false;
+ALTER TABLE gcd_indicia_publisher ADD `deleted` bool NOT NULL DEFAULT false;
