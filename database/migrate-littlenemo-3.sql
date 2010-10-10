f2085406b683307cad3ea0ea962d0526defdc8d3
Jochen Garcke <gcdmail@garcke.de>
2010-10-10 15:20:52 +0000

    rev 583 removed '/' in css links to work with css-compres rev 585 add option to make compressed compare view the default behaviour

--- database/migrate-littlenemo-3.sql
+++ database/migrate-littlenemo-3.sql
@@ -5,7 +5,8 @@ DELETE FROM gcd_cover where has_image = 0;
 ALTER TABLE gcd_cover DROP COLUMN has_image;
 ALTER TABLE gcd_cover ADD `deleted` bool NOT NULL DEFAULT false,
     ADD INDEX(deleted);
-ALTER TABLE gcd_indexer ADD `notify_on_approve` bool NOT NULL DEFAULT false;
+ALTER TABLE gcd_indexer ADD `notify_on_approve` bool NOT NULL DEFAULT false,
+   ADD `collapse_compare_view` bool NOT NULL DEFAULT false;
 ALTER TABLE gcd_brand ADD `deleted` bool NOT NULL DEFAULT false,
     ADD INDEX(deleted);
 ALTER TABLE gcd_issue ADD `deleted` bool NOT NULL DEFAULT false,
