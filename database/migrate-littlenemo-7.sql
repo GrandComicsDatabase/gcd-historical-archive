97c9194f3791c146a1ea8f4186d07b3242fe3f99
Ralf Haring <ralf.haring@gmail.com>
2010-10-17 18:00:33 +0000

    import old site issue change history - http://reviews.comics.org/r/545

--- database/migrate-littlenemo-7.sql
+++ database/migrate-littlenemo-7.sql
@@ -6,9 +6,13 @@ update oi_changeset set modified = created where change_type = 7 and approver_id
 update oi_publisher_revision set modified = created where created < "2009-12-07";
 drop table log_publisher;
 
-
 -- littlenemo-6b.sql and apps/gcd/migration/history_series.py must be run first
--- updates all the old site cover changesets as well
 update oi_changeset set modified = created where created < "2009-12-07";
 update oi_series_revision set modified = created where created < "2009-12-07";
 drop table log_series;
+
+-- littlenemo-6c.sql and apps/gcd/migration/history_issues.py must be run first
+update oi_changeset set modified = created where created < "2009-12-07";
+update oi_issue_revision set modified = created where created < "2009-12-07";
+drop table log_issues;
+drop table log_issues2;
