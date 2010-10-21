80eb02ef9e6841d54aa6eb3403b0f562f369b082
Ralf Haring <ralf.haring@gmail.com>
2010-10-21 06:39:27 +0000

    ISBN field - http://reviews.comics.org/r/605

--- database/migrate-littlenemo-10.sql
+++ database/migrate-littlenemo-10.sql
@@ -8,3 +8,5 @@ ALTER TABLE oi_series_revision
     ADD COLUMN classification_id integer default NULL AFTER name,
     ADD FOREIGN KEY (classification_id) REFERENCES gcd_classification (id);
 
+ALTER TABLE gcd_issue ADD COLUMN isbn VARCHAR(32) NOT NULL DEFAULT '';
+ALTER TABLE oi_issue_revision ADD COLUMN isbn VARCHAR(32) NOT NULL DEFAULT '';
