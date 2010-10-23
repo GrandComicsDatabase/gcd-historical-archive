f06d91382a6836cb5b62c4e2921ed633be0a04d5
Henry Andrews <hha1@cornell.edu>
2010-10-23 22:20:32 +0000

    With apologies, reverting out changes I accidentally checked in with the IMP code. This was me messing around with migration changes, and not necessarily even what I intend to submit for review.  Sorry!  I thought they were in my other tree.

--- database/migrate-littlenemo-6a.sql
+++ database/migrate-littlenemo-6a.sql
@@ -1,114 +1,38 @@
 -- copy table so we don't modify the final dump directly
-create table log_publisher (primary key (id)) engine=MyISAM
-    select * from GCDOnline.LogPublishers;
-
+create table log_publisher select * from GCDOnline.LogPublishers;
 -- drop columns won't need for porting
-alter table log_publisher drop column connection,
-                          drop column parentid,
-                          drop column master,
-                          drop column actiontype,
-                          add column sort_old int(11) default null,
-                          add column sort_new int(11) default null,
-                          add column userid_new int(11) default null,
-                          add column modified_new date default null,
-                          add column modifiedtime_new time default null,
-                          add index (sort_old),
-                          add index (sort_new),
-                          modify column id bigint(11) unsigned NOT NULL
-                            auto_increment;
+alter table log_publisher drop column connection, drop column parentid, drop column master, drop column actiontype;
 
 -- set Ray's 2nd login to his first login
 update log_publisher set userid = 49 where userid = 299;
 -- set to Anonymous User
 update log_publisher set userid = 249 where userid is null;
-
 -- set to (unknown)
 update log_publisher set countryid = 249 where countryid is null;
 update log_publisher set countryid = 249 where countryid = 0;
-
 -- clean up
-update log_publisher set notes = '' where notes is null;
-update log_publisher set web = '' where web is null;
-
--- Set up the change records representing the final pre-migration state.
-SET @anon=(SELECT id FROM auth_user WHERE username='anon');
-INSERT INTO log_publisher
-        (PubName, Notes, YearBegan, YearEnded, CountryID, Web, UserID, PublisherID)
-    SELECT 
-        PubName, Notes, YearBegan, YearEnded, CountryID, Web, @anon, ID
-    FROM GCDOnline.publishers;
-
--- don't port publishers we don't know about, and don't port imprint changes
--- Do this after adding current state to make sure we don't re-import junk
--- that was cleaned up by the data migration.
-DELETE lp FROM log_publisher lp LEFT OUTER JOIN
-               gcd_publisher gp ON lp.publisherid = gp.id
-    WHERE gp.id IS NULL OR gp.is_master = 0;
-
--- Anything that's left is potentially a useful row, so now adjust the user IDs
--- and timestamps to make these new-value rows instead of old-value rows.
--- The idea here is to give each row two counters that capture the off-by-one-ness
--- of the current situation.  Currently, we have the oldest possible value
--- (version 0) of an object associated with the user and time of version 1.
--- So we set the old counter to 1 for that object and the new counter to 0.
--- Then we do a self-join setting those two counters equal to each other, and
--- assign the userid and times from the old side of the join to the new side.
--- For version 0, there is a new side (new counter 0) but no old side
--- (old counter 0) because we do not know who added the object.
--- For the last version Z, there is an old side (old counter Z+1) but no new
--- side.  This is OK because this last version was inserted directly from
--- the final state of the display tables with the anonymous user and a time
--- of the server migration.  These placeholder users and times are correctly
--- discarded.
-
-SET @sort_old=1;
-SET @sort_new=0;
-UPDATE log_publisher SET sort_old=@sort_old:=@sort_old + 1,
-                         sort_new=@sort_new:=@sort_new + 1
-    ORDER BY publisherid, id;
-
--- Left join with this table order because we want the pairing where the new
--- counter is smallest to be absent, as there is nothing from which to assign
--- it a new user and times.  We will set it to anon and a suitably old placeholder
--- in a separate step later.
-UPDATE log_publisher new LEFT OUTER JOIN log_publisher old
-    ON old.sort_old=new.sort_new
-    SET new.userid_new=old.userid,
-        new.modified_new=old.modified,
-        new.modifiedtime_new=old.modifiedtime
-    WHERE old.publisherid=new.publisherid;
-
--- Now reverse the direction of the join and look for either the actual zero row,
--- for which the old side will be NULL, or mismatches in source ids, in which case
--- the old side will be the previous publisher and the new side will be the zero row
--- of the next publisher.
-SET @first_day='1901-01-01';
-SET @first_time='00:00:00';
-UPDATE log_publisher old LEFT OUTER JOIN log_publisher new
-    ON old.sort_old=new.sort_new
-    SET new.userid_new=@anon,
-        new.modified_new=@first_day,
-        new.modifiedtime_new=@first_time
-    WHERE old.publisherid != new.publisherid OR old.publisherid IS NULL;
+update log_publisher set notes = "" where notes is null;
+update log_publisher set web = "" where web is null;
+-- don't port publishers we don't know about
+delete from log_publisher where publisherid is null;
+delete from log_publisher where publisherid not in (select id from gcd_publisher);
+-- don't port imprint changes
+delete from log_publisher where publisherid in (select id from gcd_publisher where is_master = 0);
 
 -- delete duplicate rows where it is a dupe if the "group by" columns are the same
-CREATE TABLE publisher_helper (id int(11) PRIMARY KEY)
-    SELECT MIN(id) as id FROM log_publisher
-    GROUP BY pubname, notes, yearbegan, yearended, countryid, publisherid, web;
-
-DELETE lp FROM log_publisher lp LEFT OUTER JOIN publisher_helper ph ON lp.id=ph.id
-    WHERE ph.id IS NULL;
+delete log_publisher from log_publisher left outer join (
+   select min(id) as id,pubname,notes,yearbegan,yearended,countryid,modified,modifiedtime,userid,publisherid,web 
+   from log_publisher group by pubname,notes,yearbegan,yearended,countryid,publisherid,web
+) as keeprows on log_publisher.id = keeprows.id where keeprows.id is NULL;
 
 -- 1865 records reduced to 823 (56% less)
 
 -- use 2009-11-01 00:00:00 as placeholder
 update log_publisher set modified = "2009-11-01" where modified is null;
-
--- update a few times manually where some publishers had a mix of records
--- with dates and times and some without.
+-- update a few times manually where some publishers had a mix of records with dates and times and some without.
 -- publishers with only null dates and times will be handled in the python script
-update log_publisher set modifiedtime = "00:00:00"
-    where id in (1865,1870,1872,1875,1876,1877,161,1359,1590,1315);
+update log_publisher set modifiedtime = "00:00:00" where id in (1865,1870,1872,1875,1876,1877,161,1359,1590,1315);
 update log_publisher set modifiedtime = "00:00:01" where id in (1360,1164);
 update log_publisher set modifiedtime = "00:00:02" where id in (1361);
 
+-- now run apps/gcd/migration/history_publisher and then littlenemo-7.sql
--- database/migrate-littlenemo-6b.sql
+++ database/migrate-littlenemo-6b.sql
@@ -1,73 +1,27 @@
 -- copy table so we don't modify the final dump directly
-CREATE TABLE log_series (
-    PRIMARY KEY (id)
-) SELECT * FROM GCDOnline.LogSeries;
-
--- drop columns won't need for porting, add columns for shifting.
-ALTER TABLE log_series
-    DROP COLUMN frst_iss,
-    DROP COLUMN included,
-    DROP COLUMN last_iss,
-    DROP COLUMN pubdates,
-    DROP COLUMN actiontype,
-    ADD COLUMN indexer_id int(11) default NULL,
-    ADD COLUMN modified_new datetime default NULL,
-    ADD COLUMN sort_old int(11) default NULL,
-    ADD COLUMN sort_new int(11) default NULL,
-    ADD INDEX (sort_old),
-    ADD INDEX (sort_new),
-    MODIFY COLUMN id bigint(11) unsigned NOT NULL auto_increment;
-
-SET @anon=(SELECT id FROM auth_users WHERE username='auto');
-SET @unknown_pub=(SELECT id FROM gcd_publisher WHERE name='(unknown)');
-SET @unknown_country=(SELECT code FROM gcd_country WHERE name = '(unknown)');
-
--- ISO 639-2 defines a code for "undetermined" language.
-INSERT INTO gcd_language (code, name) VALUES ('und', '(undetermined)');
-SET @undetermined_language='und';
-
--- Set up the change records representing the final pre-migration state.
-INSERT INTO log_series
-        (Bk_Name, PubID, CounCode, LangCode, Format, Tracking, Pub_Note, Notes,
-         Yr_Began, Yr_Ended, ImprintID, UserID)
-    SELECT
-        Bk_Name, PubID, CounCode, LangCode, Format, Tracking, Pub_Note, Notes,
-        Yr_Began, Yr_Ended, imprint_id, @anon
-    FROM GCDOnline.series;
+create table log_series select * from GCDOnline.LogSeries;
+-- drop columns won't need for porting
+alter table log_series drop column frst_iss, drop column included, drop column last_iss, drop column pubdates, drop column actiontype;
 
 -- set Ray's 2nd login to his first login
-UPDATE log_series SET userid=49 WHERE userid = 299;
+update log_series set userid = 49 where userid = 299;
 -- set to Anonymous User
-UPDATE log_series SET userid=@anon WHERE userid IS NULL;
-
+update log_series set userid = 249 where userid is null;
 -- don't port series we don't know about
-DELETE ls FROM log_series ls LEFT OUTER JOIN gcd_series gs ON ls.seriesid=gs.id
-    WHERE ls.seriesid IS NULL;
-
+delete from log_series where seriesid is null;
+delete from log_series where seriesid not in (select id from gcd_series);
 -- set country codes to country id if known or to unknown otherwise
-UPDATE log_series ls LEFT OUTER JOIN gcd_country gc ON ls.councode = gc.code
-    SET councode=@unknown_country WHERE gc.code IS NULL;
-    WHERE councode NOT IN (SELECT code FROM gcd_country) OR councode IS NULL;
-
-UPDATE log_series ls INNER JOIN gcd_country gc ON ls.councode=gc.code
-    SET ls.councode = gc.id;
-
+update log_series set councode = 249 where councode is null;
+update log_series set councode = 249 where councode not in (select code from gcd_country);
+update log_series set councode = (select id from gcd_country where code = log_series.councode) where councode != 249;
 -- set language codes to language id if known or to unknown otherwise
-UPDATE log_series ls LEFT OUTER JOIN gcd_language gl ON ls.langcode = gl.code
-    SET ls.langcode=@undetermined_language WHERE gl.code IS NULL;
-
-update log_series set langcode = 146
-    where langcode not in (select code from gcd_language);
-update log_series set langcode = (
-        select id from gcd_language where code = log_series.langcode
-    ) where langcode != 146;
-
+insert into gcd_language (id, code, name) values (146, "zzz", "(unknown)");
+update log_series set langcode = 146 where langcode is null;
+update log_series set langcode = 146 where langcode not in (select code from gcd_language);
+update log_series set langcode = (select id from gcd_language where code = log_series.langcode) where langcode != 146;
 -- set publisher to "unknown" and imprint to null when unknown
-update log_series set pubid = 6691
-    where pubid not in (select id from gcd_publisher where is_master = 1);
-update log_series set imprintid = null
-    where imprintid not in (select id from gcd_publisher where is_master = 0);
-
+update log_series set pubid = 6691 where pubid not in (select id from gcd_publisher where is_master = 1);
+update log_series set imprintid = null where imprintid not in (select id from gcd_publisher where is_master = 0);
 -- misc clean up
 update log_series set bk_name = "" where bk_name is null;
 update log_series set format = "" where format is null;
@@ -78,18 +32,13 @@ update log_series set yr_began = 0 where yr_began is null;
 
 -- delete duplicate rows where it is a dupe if the "group by" columns are the same
 delete log_series from log_series left outer join (
-    select min(id) as id, bk_name, councode, format, langcode,
-           modified, modtime, notes, pubid, pub_note, seriesid,
-           tracking, yr_began, yr_ended, userid, imprintid
-    from log_series
-    group by bk_name, councode, format, langcode,
-             notes, pubid, pub_note, seriesid,
-             tracking, yr_began, yr_ended, imprintid
+   select min(id) as id, bk_name, councode, format, langcode, modified, modtime, notes, pubid, pub_note, seriesid, tracking, yr_began, yr_ended, userid, imprintid
+   from log_series group by bk_name, councode, format, langcode, notes, pubid, pub_note, seriesid, tracking, yr_began, yr_ended, imprintid
 ) as keeprows on log_series.id = keeprows.id where keeprows.id is NULL;
 
 -- 18884 records reduced to 16481 (13% less)
 
--- use 2009-11-01 00:00:00 as placeholder
--- (max date in table is 2009-10-01, first new site date is 2009-12-07)
+-- use 2009-11-01 00:00:00 as placeholder (max date in table is 2009-10-01, first new site date is 2009-12-07)
 update log_series set modified = "2009-11-01" where modified is null;
 
+-- now run apps/gcd/migration/history_series and then littlenemo-7.sql
