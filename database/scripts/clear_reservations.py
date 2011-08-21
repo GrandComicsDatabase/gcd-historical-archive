cd422222f2436b01ea3f60a6bee75f17717e1008
Jochen Garcke <gcdmail@garcke.de>
2011-08-21 21:24:02 +0000

    current clearing script

--- database/scripts/clear_reservations.py
+++ database/scripts/clear_reservations.py
@@ -1,23 +1,30 @@
 from apps.oi import states
+from apps.oi.models import *
+from datetime import datetime, timedelta
 
 SCRIPT_DEBUG = False
 
-def force_old_reservations(user):
-    for change in user.changesets.all():
-        if change.state == states.OPEN:
+def force_old_reservations(changes):
+    for change in changes:
             if change.storyrevisions.count():
                 change.submit(notes='This is an automatic submit for an '
-                  'old reservation from an inactive indexer. The present data '
+                  'old and inactive reservation. IMPORTANT: The present data '
                   'is from the old OI, it is live in the db, but parts may be '
-                  'missing. The submit cannot be sent back to the indexer. If '
-                  'the data needs correction please reserve and edit after '
-                  'approval. Approval is the default action.')
+                  'missing. If the data needs corrections please reserve and '
+                  'edit after approval. Approval is the default action.')
                 print (change, change.id, change.storyrevisions.count())
             else:
-                change.discard(user)
+                change.discard(change.indexer)
                 print (change, change.id)
 
 
+def clear_reservations_nine_weeks():
+    clearing_date = datetime.today()-timedelta(weeks=9)
+    changes = Changeset.objects.filter(created__lt=clearing_date, 
+                                       state=states.OPEN)\
+                               .exclude(change_type=CTYPES['cover'])
+    clear_reservations(changes)
+    
 def clear_old_reservations(user):
     if SCRIPT_DEBUG:
         for changeset in user.changesets.filter(state=states.OPEN):
@@ -25,8 +32,10 @@ def clear_old_reservations(user):
                 print (changeset, changeset.id,
                        changeset.storyrevisions.count())
                 return
+    clear_reservations(user.changesets.filter(state=states.OPEN))
 
-    for changeset in user.changesets.filter(state=states.OPEN):
+def clear_reservations(changes):
+    for changeset in changes:
         changed = False
         if changeset.inline():
             revision = changeset.inline_revision()
@@ -38,33 +47,32 @@ def clear_old_reservations(user):
         if changeset.coverrevisions.count() == 0:
             revision.compare_changes()
             if revision.is_changed:
-                print (changeset, changeset.id, revision)
-                print ("revision is changed")
+                # print (changeset, changeset.id, revision)
+                # print ("revision is changed")
                 changed = True
         else:
-            print changeset, changeset.id
+            # print changeset, changeset.id
             print ("cover upload") 
 
         if changeset.storyrevisions.count():
-            print "story"
+            # print "story"
             for story in changeset.storyrevisions.all():
                 story.compare_changes()
                 if story.is_changed:
-                     print (changeset, changeset.id, story)
-                     print ("story is changed")
+                     # print (changeset, changeset.id, story)
+                     # print ("story is changed")
                      changed = True
-        if changed:
-            changeset.submit(notes='This is an automatic submit for an '
-              'old reservation from an inactive indexer. If the change '
-              'contains useful information but needs correction please '
-              'reserve and edit after approval.')
-            print ('http://www.comics.org/changeset/%d/compare/' % 
-                   changeset.id)
-        else:
-            changeset.discard(user,notes='This is an automatic discard of '
-              'an unchanged edit by an indexer who was inactive for '
-              'three months.')
-        print (changeset, changeset.id)
+	if not SCRIPT_DEBUG:
+            if changed:
+                changeset.submit(notes='This is an automatic submit for an '
+                  'old reservation. If the change contains useful information '
+                  'but needs correction please reserve and edit after approval.')
+                print ('http://www.comics.org/changeset/%d/compare/' % 
+                       changeset.id)
+            else:
+                changeset.discard(changeset.indexer,notes='This is an automatic discard of '
+                  'an unchanged edit which was reserved for 9 weeks')
+        print (changeset, changeset.id, changed, changeset.created)
 
 def show_reservations(user):
     for changeset in user.changesets.all():
