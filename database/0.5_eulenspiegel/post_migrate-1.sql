40933d65b50835446c6f75febe4fd8e6d7863e72
Jochen Garcke <gcdmail@garcke.de>
2011-08-26 19:27:42 +0000

    rev 901 add series.has_volume

--- database/0.5_eulenspiegel/post_migrate-1.sql
+++ database/0.5_eulenspiegel/post_migrate-1.sql
@@ -1,4 +1,4 @@
--- Add issue title field.
+-- Add issue on-sale field.
 ALTER TABLE gcd_issue
     ADD COLUMN on_sale_date varchar(10) DEFAULT NULL,
     ADD COLUMN on_sale_date_uncertain tinyint(1) NOT NULL DEFAULT 0;
@@ -11,3 +11,10 @@ UPDATE gcd_issue
     SET key_date = REPLACE(key_date, '.', '-');
 UPDATE oi_issue_revision
     SET key_date = REPLACE(key_date, '.', '-');
+
+-- Add series level flag for volume
+ALTER TABLE gcd_series
+    ADD COLUMN has_volume tinyint(1) NOT NULL default 1;
+
+ALTER TABLE oi_series_revision
+    ADD COLUMN has_volume tinyint(1) NOT NULL default 1;
