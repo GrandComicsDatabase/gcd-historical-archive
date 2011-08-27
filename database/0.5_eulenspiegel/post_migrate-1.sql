565a7ed00fd7be3e559458748776bcfb46c8a484
Jochen Garcke <gcdmail@garcke.de>
2011-08-27 09:27:45 +0000

    add INDEX for on_sale_date

--- database/0.5_eulenspiegel/post_migrate-1.sql
+++ database/0.5_eulenspiegel/post_migrate-1.sql
@@ -1,7 +1,8 @@
 -- Add issue on-sale field.
 ALTER TABLE gcd_issue
     ADD COLUMN on_sale_date varchar(10) DEFAULT NULL,
-    ADD COLUMN on_sale_date_uncertain tinyint(1) NOT NULL DEFAULT 0;
+    ADD COLUMN on_sale_date_uncertain tinyint(1) NOT NULL DEFAULT 0,
+    ADD INDEX (on_sale_date);
 ALTER TABLE oi_issue_revision
     ADD COLUMN year_on_sale int(11) DEFAULT NULL,
     ADD COLUMN month_on_sale int(11) DEFAULT NULL,
