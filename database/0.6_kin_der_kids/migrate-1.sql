cb6a1c9d2d31616257efe4bd7a6386d324b6b1a8
Jochen Garcke <gcdmail@garcke.de>
2011-10-22 19:19:11 +0000

    rev 930 python model changes for reprint links

---
+++ database/0.6_kin_der_kids/migrate-1.sql
@@ -0,0 +1,22 @@
+--- migration_story_status exists, but no corresponding entries exist
+--- for new stories, might as well drop the whole table and rebuilt
+DROP TABLE migration_story_status;
+
+CREATE TABLE migration_story_status (
+    id int(11) NOT NULL auto_increment,
+    story_id int(11) NOT NULL,
+    reprint_needs_inspection tinyint(1) default NULL,
+    reprint_confirmed tinyint(1) default NULL,
+    reprint_original_notes longtext,
+    PRIMARY KEY (id),
+    KEY key_reprint_needs_inspection (`reprint_needs_inspection`),
+    KEY key_reprint_confirmed (`reprint_confirmed`),
+    KEY key_reprint_notes (`reprint_original_notes`(255))
+) ENGINE=InnoDB;
+
+INSERT INTO migration_story_status (story_id)
+    SELECT id FROM gcd_story;
+
+UPDATE migration_story_status i INNER JOIN gcd_story h ON h.id = i.story_id
+    SET i.reprint_original_notes=h.reprint_notes;
+
