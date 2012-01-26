5d504ea7ee18aba531f9bc8872b94484f4db820a
Jochen Garcke <gcdmail@garcke.de>
2012-01-26 14:40:47 +0000

    fix line

--- database/0.6_kin_der_kids/migrate-1.sql
+++ database/0.6_kin_der_kids/migrate-1.sql
@@ -1,5 +1,5 @@
---- migration_story_status exists, but no corresponding entries exist
---- for new stories, might as well drop the whole table and rebuilt
+-- migration_story_status exists, but no corresponding entries exist
+-- for new stories, might as well drop the whole table and rebuilt
 DROP TABLE migration_story_status;
 
 CREATE TABLE migration_story_status (
@@ -9,7 +9,7 @@ CREATE TABLE migration_story_status (
     reprint_confirmed tinyint(1) default NULL,
     reprint_original_notes longtext,
     PRIMARY KEY (id),
-    KEY story_id ('story_id'),
+    KEY key_story_id (`story_id`),
     KEY key_reprint_needs_inspection (`reprint_needs_inspection`),
     KEY key_reprint_confirmed (`reprint_confirmed`),
     KEY key_reprint_notes (`reprint_original_notes`(255))
