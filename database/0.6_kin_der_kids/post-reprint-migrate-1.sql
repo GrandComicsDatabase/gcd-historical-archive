ddce03fb3fa0569efb8d12d468ca6f14e1f75371
Jochen Garcke <gcdmail@garcke.de>
2012-04-10 20:19:21 +0000

    post migration set confirmed status for unchanged reprint notes

---
+++ database/0.6_kin_der_kids/post-reprint-migrate-1.sql
@@ -0,0 +1,3 @@
+--- if reprint notes stay the same during migration confirm their status
+UPDATE migration_story_status i INNER JOIN gcd_story h ON h.id = i.story_id
+    SET i.reprint_confirmed=1 where h.reprint_notes = i.reprint_original_notes;
